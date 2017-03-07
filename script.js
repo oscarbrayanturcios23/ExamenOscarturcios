3bcc92c6aa32091ea8c61a5dbf7fcdb45dbe0ad6
oscarbrayanturcios23 <oscarbrayanturcios23@gmail.com>
2017-03-07 19:11:43 +0100

    Examen

---
+++ script.js
@@ -0,0 +1,13 @@
+ var  Nombre =  angular.module ("Mensaje",[]);
+  BacoA.controller("Suma",function($scope){
+    $scope.Nombre = prompt(oscar)
+    $scope.Mensaje = function(cantidad){
+      $scope.cantidad1 + $scope.cantidad2
+    };
+  })
+ BacoA.controller("Mensaje",function($scope){
+    $scope.cantidad1 = 2;
+    $scope.cantidad2 = 1;
+    $scope.Mensaje = function(cantidad){
+      $scope.cantidad1 * $scope.cantidad2
+  })
